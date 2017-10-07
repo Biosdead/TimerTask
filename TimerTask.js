3a7898eec59c66f6ba9e79ffa15001b75dc1c8d1
Biosdead <lennonsfurtado@gmail.com>
2017-10-07 01:08:30 -0300

    Commented and play/stop btn

--- TimerTask.js
+++ TimerTask.js
@@ -1,49 +1,48 @@
-  var divId = 0;
-  var timer;
+  var divId = 0; // Id para detectar as divs dos timers e seus elementos.
+  var timer; // Timer que vai receber o setInterval para todos os timetaskers
 
-  function CreateTimer() {
-    divId++;
-    var InputTxt = document.getElementById('TimerName');
-    var divTxt = "<div class='stopwatch' id='"+divId+"'><p class='watch' id='temp"+divId+"'>(00:00:00)</p><p>"+InputTxt.value+" <br> <button type='button' name='Play' onclick='PlayTimer("+divId+");'>Play</button> <button type='button' name='Reset' onclick='ResetTimer("+divId+");'>Reset</button> <button type='button' name='Delete' onclick='DeleteTimer("+divId+");'>Delete</button></div>";
-    document.getElementById('body').innerHTML += divTxt;
-    SaveTimers(divId,divTxt);
+  function CreateTimer() { // só cria e salvas os timers
+    divId++; // incremento da ID mestre
+    var InputTxt = document.getElementById('TimerName'); // Capturo o texto do campo de input
+    var divTxt = "<div class='stopwatch' id='"+divId+"'><p class='watch' id='temp"+divId+"'>(00:00:00)</p><p>"+InputTxt.value+" <br> <button id='Play"+divId+"' type='button' name='Play' onclick='PlayTimer("+divId+");'>Play</button> <button type='button' name='Reset' onclick='ResetTimer("+divId+");'>Reset</button> <button type='button' name='Delete' onclick='DeleteTimer("+divId+");'>Delete</button></div>";
+    document.getElementById('body').innerHTML += divTxt; // Inxerta o html acima no html da pagina
+    SaveTimers(divId,divTxt); // Salva a id mestre atual e seu texto html
   }
 
   function DeleteTimer(id) { // Apaga apenas o Timer selecionado
-    localStorage.removeItem(id);
-    var elem = document.getElementById(id);
+    localStorage.removeItem(id); // remove o timer selecionado do localStorage
+    var elem = document.getElementById(id); // identifica timertask selecionado e o remove na linha seguinte
     elem.remove();
   }
 
   function PlayTimer(id) {
-    var tempId = "temp"+id;
+    var tempId = "temp"+id; // usada para selecionar o timer
     temp = document.getElementById(tempId).innerHTML;
-    console.log(temp);
-    console.log(id);
-    var segundos = Number(temp.slice(7,9)),minutos = Number(temp.slice(4,6)),horas = Number(temp.slice(1,3));
-    console.log("segundos "+segundos);
-    console.log("minutos "+minutos);
-    console.log("horas "+horas);
+    var segundos = Number(temp.slice(7,9)),minutos = Number(temp.slice(4,6)),horas = Number(temp.slice(1,3)); // Captura apenas os dados numericos do timer e os converte os valores de string para numeros
     if (timer!=null) {
-      clearInterval(timer);
+      var tempPlayId = "Play"+id; // Usado para selecionar o butão Play e colocar Start ou Stop
+      document.getElementById(tempPlayId).innerHTML = "Play";
+      clearInterval(timer); // para o setInterval assim parando o contador
       timer = null;
     }else {
-      timer = setInterval(function() {if(segundos==60){segundos=0;minutos++;}else{segundos++;}if (minutos == 60) {horas++;} update(segundos,minutos,horas,id)}, 1000);
+      timer = setInterval(function() {if(segundos==60){segundos=0;minutos++;}else{segundos++;}if (minutos == 60) {horas++;} update(segundos,minutos,horas,id)}, 1000); // chama o setInterval e atualiza o timer
+      var tempPlayId = "Play"+id;
+      document.getElementById(tempPlayId).innerHTML = "Stop";
+
     }
 
   }
 
-  function ResetTimer(id) {
+  function ResetTimer(id) { // Reseta o Timer selecionado ao sobre-escrever o texto do contador
     var tempId = "temp"+id;
     document.getElementById(tempId).innerHTML = "(00:00:00)";
     PlayTimer(id);
   }
 
-  function LoadTimers(){
-    // Carregar os Timers
-    if (localStorage.getItem("lastId") == null) {
+  function LoadTimers(){ // Carregar os Timers
+    if (localStorage.getItem("lastId") == null) { // Se o lastId é nulo, iniciar o divId mestre
       divId = 0;
-    }else {
+    }else { // Caso contrario, recuperar o lastId e varer todos as ids para plota-las no html ou para passar para a proxima div
       divId = localStorage.getItem("lastId");
       for (var i = 0; i <= divId; i++) {
         var divTxt = localStorage.getItem(""+i);
@@ -56,17 +55,16 @@
 
 
 
-  function UpdateTimers(id,data){
+  function UpdateTimers(id,data){ // Recebe e atualiza a div selecionada no localStorage a cada segundo do setInterval
     localStorage.setItem(id,data);
   }
 
-  function SaveTimers(id,data){
-    localStorage.setItem(id,data);
-    localStorage.setItem("lastId",id);
+  function SaveTimers(id,data){ // Salva no localStorage a div criada
+    localStorage.setItem(id,data); // Método exclusivo do CreateTimer
+    localStorage.setItem("lastId",id); // Salva a ultima ID para ajudar no LoadTimers
   }
 
-  function update(segundos,minutos,horas,id){
-    console.log(id);
+  function update(segundos,minutos,horas,id){ // Padroniza o timer em HH:mm:ss e a função que é chamada a cada ciclo do setInterval
     var tempId = "temp"+id;
     if (segundos<10) {
       segundos = "0"+segundos;
@@ -78,8 +76,6 @@
       horas = "0"+horas;
     }
     document.getElementById(tempId).innerHTML = "("+horas+":"+minutos+":"+segundos+")";
-
-    console.log("<div class='stopwatch' id='"+id+"'>"+document.getElementById(id).innerHTML);
     UpdateTimers(id,"<div class='stopwatch' id='"+id+"'>"+document.getElementById(id).innerHTML);
   }
 
